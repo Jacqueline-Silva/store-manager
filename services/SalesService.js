639bcbc4654ffbfe9eca1f9cbdd11a86fed460c7
Jacqueline Silva <jacque.sx@hotmail.com>
2022-07-06 23:57:30 -0300

    fix: retirando console.log

--- services/SalesService.js
+++ services/SalesService.js
@@ -7,7 +7,7 @@ const SalesService = {
   },
   getSalesId: async (id) => {
     const salesId = await SalesModel.getSalesId(id);
-    console.log('10', salesId);
+
     if (!salesId || salesId.length === 0) throw new Error('Sale not found');
 
     return salesId;
